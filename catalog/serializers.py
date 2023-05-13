cae6930aa9305e9c4c0894ddef9e5d312a927e1d
kn4y4n <kn4y4nnn@gmail.com>
2023-05-14 01:17:02 +0530

    feat: viewset inclusion

---
+++ catalog/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from .models import Category, SubCategory, Product
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class SubCategorySerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+
+    class Meta:
+        model = SubCategory
+        fields = "__all__"
+        extra_fields = ["category"]
+
+    def get_field_names(self, declared_fields, info):
+        expanded_fields = super(SubCategorySerializer, self).get_field_names(
+            declared_fields, info
+        )
+
+        if getattr(self.Meta, "extra_fields", None):
+            return expanded_fields + self.Meta.extra_fields
+        else:
+            return expanded_fields
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    subcategory = SubCategorySerializer(read_only=True)
+
+    class Meta:
+        model = Product
+        fields = "__all__"
+        extra_fields = ["subcategory"]
+
+    def get_field_names(self, declared_fields, info):
+        expanded_fields = super(ProductSerializer, self).get_field_names(
+            declared_fields, info
+        )
+
+        if getattr(self.Meta, "extra_fields", None):
+            return expanded_fields + self.Meta.extra_fields
+        else:
+            return expanded_fields
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.decorators import action, api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+from .models import Product, Category, SubCategory
+from .serializers import ProductSerializer, CategorySerializer, SubCategorySerializer
+
+
+class ProductViewset(viewsets.ModelViewSet):
+    queryset = Product.objects.filter(is_active=True)
+    serializer_class = ProductSerializer
+
+
+class CategoryViewset(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class SubCategoryViewset(viewsets.ModelViewSet):
+    queryset = SubCategory.objects.all()
+    serializer_class = SubCategorySerializer
