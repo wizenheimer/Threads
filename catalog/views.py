c2a95894cf590e9e777c0df1e5b341a22ee4999a
kn4y4n <kn4y4nnn@gmail.com>
2023-05-14 01:25:33 +0530

    feat: add pagination

--- catalog/views.py
+++ catalog/views.py
@@ -6,18 +6,22 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from .models import Product, Category, SubCategory
 from .serializers import ProductSerializer, CategorySerializer, SubCategorySerializer
+from .pagination import StandardResultsSetPagination, LargeResultsSetPagination
 
 
 class ProductViewset(viewsets.ModelViewSet):
     queryset = Product.objects.filter(is_active=True)
     serializer_class = ProductSerializer
+    pagination_class = StandardResultsSetPagination
 
 
 class CategoryViewset(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    pagination_class = LargeResultsSetPagination
 
 
 class SubCategoryViewset(viewsets.ModelViewSet):
     queryset = SubCategory.objects.all()
     serializer_class = SubCategorySerializer
+    pagination_class = LargeResultsSetPagination
