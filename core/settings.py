07bc94dbc9e0fb2539d53aa26fcd994e8118820e
kn4y4n <kn4y4nnn@gmail.com>
2023-05-14 02:49:52 +0530

    build: docker postgresql build

--- core/settings.py
+++ core/settings.py
@@ -79,6 +79,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#  Development Server
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
