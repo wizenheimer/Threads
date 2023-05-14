d5cfbac30249e6140c5cdafee2ab35f2fc868685
kn4y4n <kn4y4nnn@gmail.com>
2023-05-14 17:50:28 +0530

    feat: vectorized loader

--- catalog/management/commands/breeze.py
+++ catalog/management/commands/breeze.py
@@ -1,8 +1,6 @@
 import pandas as pd
-from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from pathlib import Path
-import csv
 from catalog.models import Product, Category, SubCategory
 
 
@@ -15,12 +13,6 @@ class Command(BaseCommand):
             "-p",
             type=str,
             help="Indicates the relative path of the CSV file",
-        )
-        parser.add_argument(
-            "--offset",
-            "-o",
-            type=int,
-            help="Indicates the offset",
         )
         parser.add_argument(
             "--wipe",
@@ -39,7 +31,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         path = Path(kwargs.get("path", None))
-        offset = kwargs.get("offset", 1)
         wipe = kwargs.get("wipe", True)
 
         if path is None:
@@ -54,36 +45,45 @@ class Command(BaseCommand):
             self.style.SUCCESS("Path discovered.. performing data sanitization.,.")
         )
 
-        cleaned_csv = self.clean_up(path=absolute_path)
+        cleaned_csv_path = self.clean_up(path=absolute_path)
         self.stdout.write(self.style.SUCCESS("Data cleaned up. Preparing to load..."))
 
-        count = 0
         # TODO: switch to vectorized solution
-        with open(cleaned_csv, encoding="utf8") as file:
-            reader = csv.reader(file)
-            for _ in range(0, offset):
-                next(reader)  # Advance past the header
+        if wipe:
+            SubCategory.objects.all().delete()
+            Category.objects.all().delete()
+            Product.objects.all().delete()
 
-            if wipe:
-                SubCategory.objects.all().delete()
-                Category.objects.all().delete()
-                Product.objects.all().delete()
+        df = pd.read_csv(cleaned_csv_path)
 
-            for row in reader:
-                count += 1
-                category = Category.objects.get_or_create(
-                    title=row[1],
-                )[0]
-                sub_category = SubCategory.objects.get_or_create(
-                    title=row[2],
-                    category=category,
-                )[0]
-                product = Product.objects.get_or_create(
-                    brand=row[5],
-                    product_type=row[3],
-                    alt_type=row[4],
-                    product_url=row[5],
-                    title=row[6],
-                    sub_category=sub_category,
-                )
-        self.stdout.write(self.style.SUCCESS(f"Loaded {count} into the model"))
+        category = df["Category"].to_numpy()
+        sub_category = df["Sub Category"].to_numpy()
+        product_type = df["Product Type"].to_numpy()
+        alt_type = df["Alt Type"].to_numpy()
+        brand = df["Brand Name"].to_numpy()
+        title = df["Name"].to_numpy()
+        product_url = df["URL"].to_numpy()
+
+        for cat, sub_cat, prod_type, alt, brand_name, prod_title, url in zip(
+            category, sub_category, product_type, alt_type, brand, title, product_url
+        ):
+            category = Category.objects.get_or_create(
+                title=cat,
+            )[0]
+            sub_category = SubCategory.objects.get_or_create(
+                title=sub_cat,
+                category=category,
+            )[0]
+            product = Product.objects.get_or_create(
+                brand=brand_name,
+                product_type=prod_type,
+                alt_type=alt,
+                product_url=url,
+                title=prod_title,
+                sub_category=sub_category,
+            )
+
+        self.stdout.write(self.style.SUCCESS(f"Loaded csv into the model"))
+        self.stdout.write(self.style.SUCCESS(f"Cleaning up.."))
+        cleaned_csv_path.unlink()
+        self.stdout.write(self.style.SUCCESS(f"Done."))
